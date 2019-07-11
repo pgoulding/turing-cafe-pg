69b29a2cb4d31574893b39f7c0a39561276c5902
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-11 09:54:23 -0600

    add reservcation container and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import fetchReservations from './apiFetch'
+import ReservationContainer from './ReservationContainer'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      reservations:[]
+    }
+  }
+
+  componentDidMount(){
+    fetchReservations()
+      .then(reservations => this.setState({reservations:[...reservations]}))
+      .catch(error => console.error(error))
+  }
+
+
   render() {
     return (
-      <div className="App">
+      <main className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
 
         </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
+        {this.state.reservations && <ReservationContainer data={this.state.reservations} /> }
+      </main>
     )
   }
 }
